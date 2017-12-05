f788eb2aad88c0291cd094ce94c2a0ebf3d929a8
yzg <shirasagi13@gmail.com>
2017-12-06 01:49:50 +0900

    Initial commit

---
+++ GameLiftIntegrationExample/Program.fs
@@ -0,0 +1,45 @@
+﻿// F# の詳細については、http://fsharp.org を参照してください
+// 詳細については、'F# チュートリアル' プロジェクトを参照してください。
+
+open Aws.GameLift.Server
+open Aws.GameLift.Server.Model
+open System.Diagnostics
+open System.Collections.Generic
+open System
+
+[<EntryPoint>]
+let main argv = 
+    try
+        let listeningPort = 7777
+
+        let initSDKOutcome = GameLiftServerAPI.InitSDK()
+        if initSDKOutcome.Success then
+            let onStartGameSession = ProcessParameters.OnStartGameSessionDelegate(fun (gameSession : GameSession) ->
+                GameLiftServerAPI.ActivateGameSession() |> ignore
+            )
+
+            let onProcessTerminate = ProcessParameters.OnProcessTerminateDelegate(fun () -> 
+                GameLiftServerAPI.ProcessEnding() |> ignore
+            )
+            let onHealthCheck = ProcessParameters.OnHealthCheckDelegate(fun () -> true)
+
+            let processParameters = new ProcessParameters(
+                                        onStartGameSession,
+                                        onProcessTerminate,
+                                        onHealthCheck,
+                                        listeningPort,
+                                        new LogParameters(new List<string>(["/local/game/logs/myserver.log"])))
+
+            let processReadyOutcome = GameLiftServerAPI.ProcessReady(processParameters)
+            if processReadyOutcome.Success then
+               printfn "ProcessReadySuccess"
+            else
+               printfn "ProcessReadyFailure : %A" processReadyOutcome.Error
+        else
+            printfn "InitSDK failure : %A" initSDKOutcome.Error
+        
+        Console.Read()
+
+        0 // 整数の終了コードを返します
+    finally
+        GameLiftServerAPI.Destroy() |> ignore
